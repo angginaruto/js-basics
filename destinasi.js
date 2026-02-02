0c594ffeea64761baa04a573f241ba2f560c13dc
angginaruto <angginaruto774@gmail.com>
2026-02-02 09:32:48 +0700

    mengubah nama file dan isinya

--- destinasi.js
+++ destinasi.js
@@ -1,10 +1,13 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync()
 
-console.log("Hello AI")
-function greet(name){
-    console.log(`Halo ${name} selamat pagi`)
+const destinasi = prompt("Masukan nama destinasi : ")
+const jarak_kilometer = Number(prompt("Masukan jarak ke destinasi dalam kilometer : "))
+const kecepatan = Number(prompt("Masukan kecepatan kendaraan dalam km/jam : "))
+
+function menuju_destinasi(destinasi, jarak_kilometer, kecepatan){
+    const sampai_dalam = jarak_kilometer/kecepatan
+    console.log(`Maka Anda akan sampai ke ${destinasi} dalam waktu ${sampai_dalam} jam`)
 }
 
-const nama = prompt("Masukan nama Anda : ")
-greet(nama)
+menuju_destinasi(destinasi, jarak_kilometer, kecepatan)
