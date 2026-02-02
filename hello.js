1ec419dca497f372b62057f51d7d483be380f815
angginaruto <angginaruto774@gmail.com>
2026-02-02 08:12:09 +0700

    penambahan function greet

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
 console.log("Hello AI")
+function greet(name){
+    console.log(`Halo ${name} selamat pagi`)
+}
+
+const nama = prompt("Masukan nama Anda : ")
+greet(nama)
